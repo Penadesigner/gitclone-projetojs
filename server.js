c4ca03fc2bb0ddfce226f69f12f9469c029c1555
Guilherme <gui.domenes@gmail.com>
2024-07-07 22:02:36 -0300

    atualizado com aula 12 funcionando

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
 const express = require('express');
 const app = express();
-const routes = require('./route');
-//incluir caminho absoluto dos arquivos
+const routes = require('./routes');
 const path = require('path');
 
-// Configuraçoes
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(routes);
+
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.set('views', path.resolve(__dirname, 'src', 'views'));
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
+app.use(routes);
 
-/* conexão */
 app.listen(3000, () => {
-    console.log('Escutando http://localhost:3000')
-})
-
+  console.log('Acessar http://localhost:3000');
+  console.log('Servidor executando na porta 3000');
+});
--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,3 +1,7 @@
-exports.paginaHome = (req, res) => {
-    res.render('home')
-}
+exports.paginaInicial = (req, res) => {
+    res.render('index');
+  };
+  
+  exports.trataPost = (req, res) => {
+    res.send('Ei, sou sua nova rota de POST.');
+  };

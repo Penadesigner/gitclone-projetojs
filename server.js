318d6d176907308884038bd93f0745fb734fc31b
Guilherme <gui.domenes@gmail.com>
2024-07-09 13:57:45 -0300

    finalizado pronto para uso

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 require('dotenv').config();
-
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const routes = require('./routes');
-const path = require('path');
-const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
-
-
 mongoose.connect(process.env.CONNECTIONSTRING)
   .then(() => {
     app.emit('pronto');
   })
   .catch(e => console.log(e));
-
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
+const routes = require('./routes');
+const path = require('path');
 const csrf = require('csurf');
+const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'public')));
 
 const sessionOptions = session({
   secret: 'akasdfj0út23453456+54qt23qv  qwf qwer qwer qewr asdasdasda a6()',
@@ -29,22 +30,17 @@ const sessionOptions = session({
     httpOnly: true
   }
 });
-app.use(csrf());
-app.use(checkCsrfError);
-app.use(csrfMiddleware);
-
 app.use(sessionOptions);
 app.use(flash());
-
-app.use(express.urlencoded({ extended: true }));
-
-app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
+app.use(csrf());
 // Nossos próprios middlewares
 app.use(middlewareGlobal);
+app.use(checkCsrfError);
+app.use(csrfMiddleware);
 app.use(routes);
 
 app.on('pronto', () => {

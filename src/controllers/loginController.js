244184a8438d550f2f24f065676b5a247eccb3dd
Guilherme Domenes Santana <guilherme.santana@clude.com.br>
2024-07-15 16:03:28 -0300

    login page

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,7 +1,10 @@
+const Login = require('../models/loginModel')
+
 exports.index = (req, res) => {
     res.render('login');
 };
 
 exports.register = (req, res) => {
+  const login = new Login (req.body)
   res.send(req.body);
 };

5e1b36512436496ac322e8ad8cca69172ec9e846
Guilherme Domenes Santana <guilherme.santana@clude.com.br>
2024-07-17 16:54:32 -0300

    login page

--- src/models/loginModel.js
+++ src/models/loginModel.js
@@ -17,8 +17,14 @@ class Login {
 
 async register() {
   this.valida();
-  if(this.errors.length > 0) 
-    return;
+  if(this.errors.length > 0) return;
+
+  try {
+    this.user = await LoginModel.create(this.body);
+  }
+  catch(e) {
+    console.log(e);
+  }
 }
 
 valida() {
